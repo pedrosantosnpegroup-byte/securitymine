a330b52d77457ba8801192c6a0243db197e1fe22
pedrosantosnpegroup-byte <pedrosantosnpegroup@gmail.com>
2026-07-08 15:23:11 -0300

    Delete DDSjogo directory

--- DDSjogo/popup.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".area").forEach(area => {
-    area.style.setProperty("--img", `url('${area.dataset.img}')`);
-});
-
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js");
-}
