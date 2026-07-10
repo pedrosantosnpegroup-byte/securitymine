fe3d16eb89fe7bfdff7811dd282ee01ac854495d
pedrosantosnpegroup-byte <pedrosantosnpegroup@gmail.com>
2026-07-10 16:37:08 -0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,3 @@
 document.querySelectorAll(".area").forEach(area => {
     area.style.setProperty("--img", `url('${area.dataset.img}')`);
 });
-
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js");
-}
